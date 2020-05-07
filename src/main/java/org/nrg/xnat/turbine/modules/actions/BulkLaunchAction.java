56bc3e52d9764d81d7500a5dd7c7cdb50e4ca652
Kate Alpert <kate@radiologics.com>
2020-05-07 13:46:13 -0600

    Whitespace changes

--- src/main/java/org/nrg/xnat/turbine/modules/actions/BulkLaunchAction.java
+++ src/main/java/org/nrg/xnat/turbine/modules/actions/BulkLaunchAction.java
@@ -32,140 +32,137 @@ import com.google.common.collect.Lists;
 
 @SuppressWarnings("unused")
 public class BulkLaunchAction extends DisplaySearchAction {
-	   static Logger logger = Logger.getLogger(BulkLaunchAction.class);
+    static Logger logger = Logger.getLogger(BulkLaunchAction.class);
 
-
-	public void doPerform(RunData data, Context context)
-	{
-		try {
-		   // doPreliminaryProcessing(data,context);
-		    final UserI user = TurbineUtils.getUser(data);
-		    String search_xml = data.getParameters().getString("search_xml");
-		    if(search_xml==null){
-		    	data.setMessage("Your search result has expired.  Please resubmit your query. ");
-		        data.setScreenTemplate("Error.vm");
-		        return;
-		    }
-		    search_xml = search_xml.replaceAll("%", "%25");
-		    search_xml = URLDecoder.decode(search_xml, "UTF-8");
-		    search_xml = StringUtils.replace(search_xml, ".close.", "/");
-
-		    final String startTag = "<xdat:search_where";
-		    final String endTag = "</xdat:search_where>";
-		    String whereClause = StringUtils.defaultIfBlank(StringUtils.substringBetween(search_xml, startTag, endTag),
-					"");
-		    if (StringUtils.isNotBlank(whereClause)) {
-		    	whereClause = startTag + whereClause + endTag;
-			}
-
-			context.put("xss",this.buildNewSearchXML(search_xml, user, whereClause, data));
-			super.doPreliminaryProcessing(data, context);
-			data.setScreenTemplate(getScreenTemplate());
-
-			doFinalProcessing(data,context);
-		} catch (SearchTimeoutException e) {
-	        logger.error(e);
-	        data.setMessage(e.getMessage());
-	        data.setScreenTemplate("Index.vm");
-	    } catch (XFTInitException e) {
-	        this.error(e, data);
-		} catch (ElementNotFoundException e) {
-	        this.error(e, data);
-		} catch (DBPoolException e) {
-	        this.error(e, data);
-		}catch (IllegalAccessException e){
-	        data.setMessage("The user does not have access to this data.");
-	        data.setScreenTemplate("Error.vm");
-	        data.getParameters().setString("exception", e.toString());
-		}catch (InvalidSearchException e){
-	        data.setMessage("You specified an invalid search condition: " + e.getMessage());
-	        data.setScreenTemplate("Error.vm");
-		} catch (Exception e) {
-			e.printStackTrace();
-	        this.error(e, data);
-		}
-	}
-
-	 private String buildNewSearchXML(final String search_xml, UserI user, final String whereClause, final RunData data) throws Exception{;
-	 	 final StringReader sr = new StringReader(search_xml);
-         final InputSource is = new InputSource(sr);
-         final SAXReader reader = new SAXReader(user);
-         final XFTItem item = reader.parse(is);
-         final XdatStoredSearch search = new XdatStoredSearch(item);
-         final DisplaySearch ds = search.getCSVDisplaySearch(user);
-
-            if (ds==null) {
-                throw new SearchTimeoutException("BuldLaunchAction Session Expired: The previously performed search has timed out.");
+    public void doPerform(RunData data, Context context) {
+        try {
+            // doPreliminaryProcessing(data,context);
+            final UserI user = TurbineUtils.getUser(data);
+            String search_xml = data.getParameters().getString("search_xml");
+            if (search_xml == null) {
+                data.setMessage("Your search result has expired.  Please resubmit your query. ");
+                data.setScreenTemplate("Error.vm");
+                return;
             }
-            String rootElementName = ds.getRootElement().getFullXMLName();
-            if (rootElementName == null) {
-				 throw new Exception("Invalid value submitted.");
-			}
-            //Load search results into a table
-            org.nrg.xft.XFTTable table = (org.nrg.xft.XFTTable)ds.execute(null,user.getLogin());
+            search_xml = search_xml.replaceAll("%", "%25");
+            search_xml = URLDecoder.decode(search_xml, "UTF-8");
+            search_xml = StringUtils.replace(search_xml, ".close.", "/");
 
-
-            //The 'session_id' value is specified as the DisplayField ID for the xnat:mrSessionData/ID field in the Display docs.
-            //This value should match the value at the header of the session id column in the previous ExampleListingActionScreen implementation.
-            String sessionIDHeader ="session_id";
-            String projectHeader  = "Project";
-            //Distinct projects
-            List<String> distinctProjectsInSearch = new ArrayList<String>();
-            table.resetRowCursor();
-            while (table.hasMoreRows()){
-                Hashtable<?, ?> row= table.nextRowHash();
-                String project = (String)row.get(projectHeader);
-                if (project == null) project = (String)row.get(projectHeader.toLowerCase());
-                if (!distinctProjectsInSearch.contains(project)) {
-                	distinctProjectsInSearch.add(project);
-                }
-            }
-            
-            String job = data.getParameters().getString("job");
-            if (job!=null && PoolDBUtils.HackCheck(job)) {
-            	throw new Exception("Invalid value submitted.");
+            final String startTag = "<xdat:search_where";
+            final String endTag = "</xdat:search_where>";
+            String whereClause = StringUtils.defaultIfBlank(StringUtils.substringBetween(search_xml, startTag, endTag),
+                    "");
+            if (StringUtils.isNotBlank(whereClause)) {
+                whereClause = startTag + whereClause + endTag;
             }
 
-            String resources = data.getParameters().getString("resources");
-            if (resources!=null && PoolDBUtils.HackCheck(resources)) {
-            	throw new Exception("Invalid value submitted.");
+            context.put("xss", this.buildNewSearchXML(search_xml, user, whereClause, data));
+            super.doPreliminaryProcessing(data, context);
+            data.setScreenTemplate(getScreenTemplate());
+
+            doFinalProcessing(data, context);
+        } catch (SearchTimeoutException e) {
+            logger.error(e);
+            data.setMessage(e.getMessage());
+            data.setScreenTemplate("Index.vm");
+        } catch (XFTInitException e) {
+            this.error(e, data);
+        } catch (ElementNotFoundException e) {
+            this.error(e, data);
+        } catch (DBPoolException e) {
+            this.error(e, data);
+        } catch (IllegalAccessException e) {
+            data.setMessage("The user does not have access to this data.");
+            data.setScreenTemplate("Error.vm");
+            data.getParameters().setString("exception", e.toString());
+        } catch (InvalidSearchException e) {
+            data.setMessage("You specified an invalid search condition: " + e.getMessage());
+            data.setScreenTemplate("Error.vm");
+        } catch (Exception e) {
+            e.printStackTrace();
+            this.error(e, data);
+        }
+    }
+
+    private String buildNewSearchXML(final String search_xml, UserI user, final String whereClause, final RunData data) throws Exception {
+        ;
+        final StringReader sr = new StringReader(search_xml);
+        final InputSource is = new InputSource(sr);
+        final SAXReader reader = new SAXReader(user);
+        final XFTItem item = reader.parse(is);
+        final XdatStoredSearch search = new XdatStoredSearch(item);
+        final DisplaySearch ds = search.getCSVDisplaySearch(user);
+
+        if (ds == null) {
+            throw new SearchTimeoutException("BuldLaunchAction Session Expired: The previously performed search has timed out.");
+        }
+        String rootElementName = ds.getRootElement().getFullXMLName();
+        if (rootElementName == null) {
+            throw new Exception("Invalid value submitted.");
+        }
+        //Load search results into a table
+        org.nrg.xft.XFTTable table = (org.nrg.xft.XFTTable) ds.execute(null, user.getLogin());
+
+
+        //The 'session_id' value is specified as the DisplayField ID for the xnat:mrSessionData/ID field in the Display docs.
+        //This value should match the value at the header of the session id column in the previous ExampleListingActionScreen implementation.
+        String sessionIDHeader = "session_id";
+        String projectHeader = "Project";
+        //Distinct projects
+        List<String> distinctProjectsInSearch = new ArrayList<String>();
+        table.resetRowCursor();
+        while (table.hasMoreRows()) {
+            Hashtable<?, ?> row = table.nextRowHash();
+            String project = (String) row.get(projectHeader);
+            if (project == null) project = (String) row.get(projectHeader.toLowerCase());
+            if (!distinctProjectsInSearch.contains(project)) {
+                distinctProjectsInSearch.add(project);
             }
-            
-            String[] resourceArray=null;
-            if(org.apache.commons.lang3.StringUtils.isNotEmpty(resources)){
-            	if(resources.contains(",")){
-            		resourceArray=resources.split(",");
-            	}
+        }
+
+        String job = data.getParameters().getString("job");
+        if (job != null && PoolDBUtils.HackCheck(job)) {
+            throw new Exception("Invalid value submitted.");
+        }
+
+        String resources = data.getParameters().getString("resources");
+        if (resources != null && PoolDBUtils.HackCheck(resources)) {
+            throw new Exception("Invalid value submitted.");
+        }
+
+        String[] resourceArray = null;
+        if (org.apache.commons.lang3.StringUtils.isNotEmpty(resources)) {
+            if (resources.contains(",")) {
+                resourceArray = resources.split(",");
             }
-            
-            String scans = (String)org.nrg.xdat.turbine.utils.TurbineUtils.GetPassedParameter("scan_types",data);
-            if (org.apache.commons.lang3.StringUtils.isNotEmpty(scans) && PoolDBUtils.HackCheck(scans)) {
-            	throw new Exception("Invalid value submitted.");
+        }
+
+        String scans = (String) org.nrg.xdat.turbine.utils.TurbineUtils.GetPassedParameter("scan_types", data);
+        if (org.apache.commons.lang3.StringUtils.isNotEmpty(scans) && PoolDBUtils.HackCheck(scans)) {
+            throw new Exception("Invalid value submitted.");
+        }
+
+        java.util.List<String> typesList = null;
+        if (org.apache.commons.lang3.StringUtils.isNotEmpty(scans)) {
+            if (resources.indexOf(",") > 0) {
+                typesList = java.util.Arrays.asList(scans.split(","));
+            } else {
+                typesList = Lists.newArrayList(scans);
             }
-            
-            java.util.List<String> typesList=null;
-            if(org.apache.commons.lang3.StringUtils.isNotEmpty(scans)){
-            	if(resources.indexOf(",")>0){
-            		typesList=java.util.Arrays.asList(scans.split(","));
-            	}else{
-            		typesList=Lists.newArrayList(scans);
-            	}
-            }
-            
-            return (new SearchXMLBuilder()).execute(distinctProjectsInSearch, rootElementName, user, whereClause,job,resourceArray==null?null:java.util.Arrays.asList(resourceArray),typesList);
-	 }
+        }
+
+        return (new SearchXMLBuilder()).execute(distinctProjectsInSearch, rootElementName, user, whereClause, job, resourceArray == null ? null : java.util.Arrays.asList(resourceArray), typesList);
+    }
 
 
+    public String getScreenTemplate() {
+        return "XDATScreen_bulk_action.vm";
+    }
 
-	public String getScreenTemplate(){
-		return "XDATScreen_bulk_action.vm";
-	}
+    public String getScreen() {
+        return "XDATScreen_bulk_action";
+    }
 
-	public String getScreen(){
-		return "XDATScreen_bulk_action";
-	}
-
-	
 
 }
 
--- src/main/java/org/nrg/xnat/turbine/modules/screens/XDATScreen_bulk_action.java
+++ src/main/java/org/nrg/xnat/turbine/modules/screens/XDATScreen_bulk_action.java
@@ -18,75 +18,75 @@ import java.util.Calendar;
 
 @SuppressWarnings("unused")
 
-public class  XDATScreen_bulk_action  extends SecureScreen {
-	   static Logger logger = Logger.getLogger(XDATScreen_bulk_action.class);
+public class XDATScreen_bulk_action extends SecureScreen {
+    static Logger logger = Logger.getLogger(XDATScreen_bulk_action.class);
 
-	    @Override
-	    protected void doBuildTemplate(RunData data, Context context) throws Exception {
-		    UserI user = TurbineUtils.getUser(data);
+    @Override
+    protected void doBuildTemplate(RunData data, Context context) throws Exception {
+        UserI user = TurbineUtils.getUser(data);
 
-			context.put("timezoneOffset",
-					Calendar.getInstance().getTimeZone().getOffset(Calendar.getInstance().getTimeInMillis()));
+        context.put("timezoneOffset",
+                Calendar.getInstance().getTimeZone().getOffset(Calendar.getInstance().getTimeInMillis()));
 
-			String job = (String)org.nrg.xdat.turbine.utils.TurbineUtils.GetPassedParameter("job",data);
-			if (org.apache.commons.lang3.StringUtils.isNotEmpty(job) && PoolDBUtils.HackCheck(job)) {
-				throw new Exception("Invalid value submitted.");
-			}else if(org.apache.commons.lang3.StringUtils.isNotEmpty(job)){
-				context.put("job", job);
-			}
+        String job = (String) org.nrg.xdat.turbine.utils.TurbineUtils.GetPassedParameter("job", data);
+        if (org.apache.commons.lang3.StringUtils.isNotEmpty(job) && PoolDBUtils.HackCheck(job)) {
+            throw new Exception("Invalid value submitted.");
+        } else if (org.apache.commons.lang3.StringUtils.isNotEmpty(job)) {
+            context.put("job", job);
+        }
 
-			String resources = (String)org.nrg.xdat.turbine.utils.TurbineUtils.GetPassedParameter("resources",data);
-			if (org.apache.commons.lang3.StringUtils.isNotEmpty(resources) && PoolDBUtils.HackCheck(resources)) {
-				throw new Exception("Invalid value submitted.");
-			}
+        String resources = (String) org.nrg.xdat.turbine.utils.TurbineUtils.GetPassedParameter("resources", data);
+        if (org.apache.commons.lang3.StringUtils.isNotEmpty(resources) && PoolDBUtils.HackCheck(resources)) {
+            throw new Exception("Invalid value submitted.");
+        }
 
-			java.util.List<String> resourceList=null;
-			if(org.apache.commons.lang3.StringUtils.isNotEmpty(resources)){
-				if(resources.indexOf(",")>0){
-					resourceList=java.util.Arrays.asList(resources.split(","));
-				}else{
-					resourceList=Lists.newArrayList(resources);
-				}
-			}
+        java.util.List<String> resourceList = null;
+        if (org.apache.commons.lang3.StringUtils.isNotEmpty(resources)) {
+            if (resources.indexOf(",") > 0) {
+                resourceList = java.util.Arrays.asList(resources.split(","));
+            } else {
+                resourceList = Lists.newArrayList(resources);
+            }
+        }
 
-			String scans = (String)org.nrg.xdat.turbine.utils.TurbineUtils.GetPassedParameter("scan_types",data);
-			if (org.apache.commons.lang3.StringUtils.isNotEmpty(scans) && PoolDBUtils.HackCheck(scans)) {
-				throw new Exception("Invalid value submitted.");
-			}
+        String scans = (String) org.nrg.xdat.turbine.utils.TurbineUtils.GetPassedParameter("scan_types", data);
+        if (org.apache.commons.lang3.StringUtils.isNotEmpty(scans) && PoolDBUtils.HackCheck(scans)) {
+            throw new Exception("Invalid value submitted.");
+        }
 
-			java.util.List<String> typesList=null;
-			if(org.apache.commons.lang3.StringUtils.isNotEmpty(scans)){
-				if(scans.indexOf(",")>0){
-					typesList=java.util.Arrays.asList(scans.split(","));
-				}else{
-					typesList=Lists.newArrayList(scans);
-				}
-			}
+        java.util.List<String> typesList = null;
+        if (org.apache.commons.lang3.StringUtils.isNotEmpty(scans)) {
+            if (scans.indexOf(",") > 0) {
+                typesList = java.util.Arrays.asList(scans.split(","));
+            } else {
+                typesList = Lists.newArrayList(scans);
+            }
+        }
 
-			// The below section is skipped if a search is passed in, ensure that everything else you need is in-context
-	    	if(TurbineUtils.HasPassedParameter("project", data) && TurbineUtils.HasPassedParameter("dataType", data)){
-	    		final String project=(String)org.nrg.xdat.turbine.utils.TurbineUtils.GetPassedParameter("project",data);
-	    		final String dataType=(String)org.nrg.xdat.turbine.utils.TurbineUtils.GetPassedParameter("dataType",data);
+        // The below section is skipped if a search is passed in, ensure that everything else you need is in-context
+        if (TurbineUtils.HasPassedParameter("project", data) && TurbineUtils.HasPassedParameter("dataType", data)) {
+            final String project = (String) org.nrg.xdat.turbine.utils.TurbineUtils.GetPassedParameter("project", data);
+            final String dataType = (String) org.nrg.xdat.turbine.utils.TurbineUtils.GetPassedParameter("dataType", data);
 
-				String sb = "<xdat:search_where method=\"AND\">" +
-						"<xdat:child_set method=\"OR\">" +
-						"<xdat:criteria override_value_formatting=\"0\">" +
-						"<xdat:schema_field>" + dataType + "/sharing/share/project</xdat:schema_field>" +
-						"<xdat:comparison_type>=</xdat:comparison_type>" +
-						"<xdat:value>" + project + "</xdat:value>" +
-						"</xdat:criteria>" +
-						"<xdat:criteria override_value_formatting=\"0\">" +
-						"<xdat:schema_field>" + dataType + "/PROJECT</xdat:schema_field>" +
-						"<xdat:comparison_type>=</xdat:comparison_type>" +
-						"<xdat:value>" + project + "</xdat:value>" +
-						"</xdat:criteria>" +
-						"</xdat:child_set>" +
-						"</xdat:search_where>";
+            String sb = "<xdat:search_where method=\"AND\">" +
+                    "<xdat:child_set method=\"OR\">" +
+                    "<xdat:criteria override_value_formatting=\"0\">" +
+                    "<xdat:schema_field>" + dataType + "/sharing/share/project</xdat:schema_field>" +
+                    "<xdat:comparison_type>=</xdat:comparison_type>" +
+                    "<xdat:value>" + project + "</xdat:value>" +
+                    "</xdat:criteria>" +
+                    "<xdat:criteria override_value_formatting=\"0\">" +
+                    "<xdat:schema_field>" + dataType + "/PROJECT</xdat:schema_field>" +
+                    "<xdat:comparison_type>=</xdat:comparison_type>" +
+                    "<xdat:value>" + project + "</xdat:value>" +
+                    "</xdat:criteria>" +
+                    "</xdat:child_set>" +
+                    "</xdat:search_where>";
 
-				context.put("xss", (new SearchXMLBuilder()).execute(Lists.newArrayList(project), dataType, user, sb,
-						job, resourceList, typesList));
-	    	}
-	    	
-	}
+            context.put("xss", (new SearchXMLBuilder()).execute(Lists.newArrayList(project), dataType, user, sb,
+                    job, resourceList, typesList));
+        }
+
+    }
 }
 
